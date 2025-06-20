105b15996f43abed3ecb5e3627b57ddc8529c21d
CS50X-RGB <rohanchatterjee866@gmail.com>
2025-06-20 19:15:36 +0530

    Axios instance Added

---
+++ po-track-frontend/core/api/axiosInstance.tsx
@@ -0,0 +1,57 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+
+import { adminToken, currentAdmin, currentUser, isSuperAdmin } from "./localStorageKeys"
+
+
+export const baseUrlExport = "http://localhost:5000/api/v1/web/"
+export const baseUrlExportNgRok = "https://2798-106-222-236-78.ngrok-free.app"
+export const localBackend = "http://localhost:5000/api/v1/web/"
+
+const instance = axios.create({
+  baseURL: baseUrlExport,
+  headers: {
+    "Content-Type": "application/json",
+   //"ngrok-skip-browser-warning": "123",
+  },
+});
+
+// Request Interceptor (unchanged)
+instance.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get(currentUser); 
+
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+   // Check if the request is multipart/form-data (file upload request)
+    if (config.data instanceof FormData) {
+      delete config.headers["Content-Type"]; // Axios will set the correct Content-Type automatically for multipart requests
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Response Interceptor (modified)
+instance.interceptors.response.use(
+  (response) => {
+    return response; // Return successful responses
+  },
+  (error) => {
+    if (error.response && error.response.status === 403) {
+      // Remove all cookies if status code is 403 (Forbidden)
+      const location = Cookies.get(currentUser) ? "/dashboard" : "/signin";
+      Cookies.remove(currentUser);
+      Cookies.remove(adminToken);
+      Cookies.remove(currentAdmin);
+      Cookies.remove(isSuperAdmin);
+      window.location.href = location;
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
